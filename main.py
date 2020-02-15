9564cfa646e9032f30264e59a177830cb516a3cd
ReyBroncas <ReyBroncas@github.com>
2020-02-15 15:20:25 +0200

    Cleaned added functions, added documentation

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import re
+
+
 def get_location_name(line):
+    """
+    Function extracts location name from string line
+    :param line: string
+    :return: string
+    """
     output_list = []
     line = line.split('\t')
-    for i in range(-1,-(len(line)+1),-1):
+    for i in range(-1, -(len(line) + 1), -1):
         if '(' in line[i]:
             continue
         elif not line[i]:
@@ -10,8 +17,13 @@ def get_location_name(line):
         output_list.append(line[i])
     return ' '.join(output_list).rstrip()
 
-def get_film_year(line):
 
+def get_film_year(line):
+    """
+    Function extracts film year from string line
+    :param line: string
+    :return: string
+    """
     num = re.compile(r'\d\d\d\d')
     film_year = num.findall(line[0])
     if film_year:
@@ -19,7 +31,13 @@ def get_film_year(line):
     else:
         return 0
 
+
 def get_film_locations(user_year):
+    """
+    Function reads data from location.list file
+    :param user_year: string
+    :return: set
+    """
     film_set = set()
     with open('locations.list', 'r', encoding="utf-8", errors='ignore') as file:
         cnt = 1
@@ -31,20 +49,17 @@ def get_film_locations(user_year):
                 film_name = line_spl[0][:line_spl[0].find('(')]
                 film_location = get_location_name(line)
                 if film_year == user_year:
-                    film_set.add((film_location,film_year,film_name))
-                # print(line, '\n----------------------')
-                # print(number)
-                # print(film)
-                # print(location)
+                    film_set.add((film_location, film_year, film_name))
             if not file.readline():
                 break
             line = file.readline()
             cnt += 1
     return film_set
 
-print(get_film_locations('2000'))
-# loc = "Naples, Campania, Italy"
-# from geopy.geocoders import Nominatim
-# geolocator = Nominatim(user_agent="map")
-# location = geolocator.geocode(loc, timeout=15)
-# print((location.latitude, location.longitude))
+
+print(get_film_locations('2016'))
+loc = "Naples, Campania, Italy"
+from geopy.geocoders import Nominatim
+geolocator = Nominatim(user_agent="map")
+location = geolocator.geocode(loc, timeout=15)
+print((location.latitude, location.longitude))
